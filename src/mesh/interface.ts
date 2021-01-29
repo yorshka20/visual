72c6deea2f5717d8d05169930005fb8ff64cad8e
yorshka <yorshka@hotmail.com>
2021-01-29 23:23:48 +0800

    新增网格类

---
+++ src/mesh/interface.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: yorshka
+ * @Date: 2021-01-29 22:35:02
+ * @Last Modified by: yorshka
+ * @Last Modified time: 2021-01-29 22:57:05
+ */
+
+import { CanvasOptions } from '@src/canvas/interface';
+
+export interface MeshOptions extends CanvasOptions {
+  [key: string]: any;
+}
---
+++ src/mesh/mesh.ts
@@ -0,0 +1,35 @@
+/*
+ * @Author: yorshka
+ * @Date: 2021-01-29 22:34:19
+ * @Last Modified by: yorshka
+ * @Last Modified time: 2021-01-29 23:03:19
+ *
+ * mesh实例，用来作为网格坐标层
+ */
+
+import { Canvas } from '@src/canvas';
+import { MeshOptions } from './interface';
+
+export default class Mesh extends Canvas {
+  // 缓冲数据集
+  private shapeBucket: Map<string, any>;
+
+  constructor(options: MeshOptions) {
+    super(options);
+
+    this.shapeBucket = new Map<string, any>();
+  }
+
+  // 录入图形，形成坐标
+  public recordShape(shape: any): void {
+    const { id } = shape;
+
+    this.shapeBucket.set(id, shape);
+  }
+
+  // 删除图形
+  public removeShape(shape: any): void {
+    const { id } = shape;
+    this.shapeBucket.delete(id);
+  }
+}
